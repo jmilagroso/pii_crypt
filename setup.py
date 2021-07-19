9e6d391662210be8f6e419c8dc8022222609b2a6
jmilagroso <j.milagroso@gmail.com>
2021-07-19 15:33:16 +0800

    Added 1.0.1 release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name='pii_crypt',
     packages=['pii_crypt'],
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
     description='Secures Personally Identifiable Information using AES (128/256 bit) key encryption.',
     author='Jay Milagroso',
